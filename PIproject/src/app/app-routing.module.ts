4682e607add20099a972bdf93914f5f648f7a856
EnioFrank <enio.jiparana@gmail.com>
2022-05-10 21:03:49 -0400

    comit2

--- PIproject/src/app/app-routing.module.ts
+++ PIproject/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'actionsheet',
+    loadChildren: () => import('./pages/actionsheet/actionsheet.module').then( m => m.ActionsheetPageModule)
   }
 ];
 
