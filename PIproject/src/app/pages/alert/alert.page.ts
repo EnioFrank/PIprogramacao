d4aa5487f5a565583a85ef003d965a8f8192e50a
EnioFrank <enio.jiparana@gmail.com>
2022-05-10 21:03:40 -0400

    segundocomit

--- PIproject/src/app/pages/alert/alert.page.ts
+++ PIproject/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCrtl: AlertController) { }
 
   ngOnInit() {
   }
 
+  async alert() {
+    const alert = await this.alertCrtl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      subHeader: 'Atencao',
+      message: 'Esta e uma mensagem de Alerta!!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async alertaMultiplosBotoes() {
+    const alert = await this.alertCrtl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta multiplos',
+      subHeader: 'Multiplos botoes',
+      message: 'Este e um alerta com multiplos botoes.',
+      buttons: ['Cancelar', 'Abrir', 'Excluir']
+    });
+
+    await alert.present();
+  }
 }
